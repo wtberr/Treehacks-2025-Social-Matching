1c079b6c2b709541eaefde5ed5710e6231415313
wtberr <wills2email@gmail.com>
2025-02-15 15:49:40 -0800

    OAuth setup, sign out button on profile page

--- sense-search/pages/profile.tsx
+++ sense-search/pages/profile.tsx
@@ -1,7 +1,7 @@
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-//just sign out button
+//just sign out buttons
 export default function Profile() {
   const { data: session, status } = useSession();
   const router = useRouter();
